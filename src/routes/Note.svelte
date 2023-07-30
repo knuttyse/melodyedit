d5b3ef27f5e48791ccb5054e596e1d58bd7d1d39
knuttyse <knuttyse@gmail.com>
2023-07-30 15:39:06 +0200

    Add Note.svelte to display a note.

---
+++ src/routes/Note.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let name = "C#4"; // Note name, e.g., C#4
+    export let duration = "1/4"; // Note duration, e.g., 1/4
+    export let velocity = 80; // Note velocity or strength, e.g., 80
+  </script>
+  
+  <style>
+    .note {
+      border: 1px solid black;
+      padding: 10px;
+      margin: 5px;
+      display: inline-block;
+    }
+    .name {
+      font-weight: bold;
+    }
+    .duration, .velocity {
+      font-style: italic;
+    }
+  </style>
+  
+  <div class="note">
+    <div class="name">Note: {name}</div>
+    <div class="duration">Duration: {duration}</div>
+    <div class="velocity">Velocity: {velocity}</div>
+  </div>
+  
