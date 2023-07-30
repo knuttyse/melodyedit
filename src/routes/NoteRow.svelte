3b90503cdfdea7815f9b2f2a22def1546c310782
knuttyse <knuttyse@gmail.com>
2023-07-30 15:43:32 +0200

    Add NoteRow.svelte to display a row of Note.

---
+++ src/routes/NoteRow.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Note from "./Note.svelte";
+    export let notes = [];
+  </script>
+  
+  <style>
+    .note-row {
+      display: flex;
+    }
+  </style>
+  
+  <div class="note-row">
+    {#each notes as note}
+      <Note {note} />
+    {/each}
+  </div>
+  
